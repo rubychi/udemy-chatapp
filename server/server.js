ca5e3f9bd6d47878f2258465729a06612b880151
RubyChi <nothelse@hotmail.com>
2017-07-23 22:40:40 +0800

    Added new feature for displaying all messages of the day

--- server/server.js
+++ server/server.js
@@ -78,8 +78,8 @@ io.on('connection', (socket) => {
         result = _.sortBy(result, 'time');
         result.forEach((item) => {
           if (_.includes(item.message, 'Send location')) {
-            const message = item.message.split(/[\s,]+/);
-            io.to(params.room).emit('newLocationMessage', generateLocationMessage(item.name, message[1], message[2], item.time));
+            const message = item.message.split(/[Send location:\s,]+/);
+            io.to(params.room).emit('newLocationMessage', generateLocationMessage(item.name, message[2], message[3], item.time));
           } else {
             io.to(params.room).emit('newMessage', {
               from: item.name,
